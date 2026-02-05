65707d99c6fc42158e194298e9f3b2c804fc2a3c
Владимир Шмидт <gr436_shmval@lab-211-5.tomtit-tomsk.ru>
2026-02-05 09:24:42 +0700

    7commit

--- js/main.js
+++ js/main.js
@@ -1,19 +1,38 @@
-let product = "Socks";
-
 let app = new Vue({
     el: '#app',
     data: {
         product: "Socks",
-        image: "./assets/vmSocks-blue-onWhite.jpg"
+        image: "./assets/vmSocks-green-onWhite.jpg",
+        altText: "A pair of socks",
+        inStock: true,
+        details: ['80% cotton', '20% polyester', 'Gender-neutral'],
+        variants: [
+            {
+                variantId: 2234,
+                variantColor: 'green',
+                variantImage: "./assets/vmSocks-green-onWhite.jpg",
+            },
+            {
+                variantId: 2235,
+                variantColor: 'blue',
+                variantImage: "./assets/vmSocks-blue-onWhite.jpg",
+            }
+        ],
+        cart: 0
+    },
+    methods: {
+        addToCart() {
+            this.cart += 1
+        },
+        updateProduct(variantImage) {
+            this.image = variantImage
+        }
     }
+    
 })
 
-data: {
-    product: "Socks",
-        image: "./assets/vmSocks-blue-onWhite.jpg",
-        altText: "A pair of socks"
 
 
-}
+
 
 
